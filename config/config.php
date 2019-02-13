0d08789de0bd4a75c61cb96df250fb8ea4cc0541
davit <david.grigorianc@gmail.com>
2019-02-13 23:54:13 +0400

    routing configuration

---
+++ config/config.php
@@ -0,0 +1,3 @@
+<?php
+//for parameters like name of site or database configuration
+Config::set('site_name','Messaging System');
--- webroot/index.php
+++ webroot/index.php
@@ -3,5 +3,6 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 
-$uri = $_SERVER['REQUEST_URI'];
-print_r($uri);
+require_once(ROOT.DS.'lib'.DS.'init.php');
+
+$router = new Router($_SERVER['REQUEST_URI']);
