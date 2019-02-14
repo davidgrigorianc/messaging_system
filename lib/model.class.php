70f4cecdd6030a637c9a175ae319a365f1e3c51f
davit <david.grigorianc@gmail.com>
2019-02-14 22:09:15 +0400

    model class added

---
+++ lib/model.class.php
@@ -0,0 +1,21 @@
+<?php
+
+abstract class Model
+{
+    /**
+     * Get the PDO database connection
+     *
+     * @return mixed
+     */
+    protected static function getDB()
+    {
+        static $db = null;
+        if ($db === null) {
+             $dsn = 'mysql:host='.Config::get('db_host').';dbname='.Config::get('db_name').';charset='.Config::get('db_char');
+            $db = new PDO($dsn, Config::get('db_user'), Config::get('db_pass'));
+            // Throw an Exception when an error occurs
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        return $db;
+    }
+}
