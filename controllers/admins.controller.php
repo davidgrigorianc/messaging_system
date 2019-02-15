bb4965089f386ccaecd8712f638353005f462389
davit <david.grigorianc@gmail.com>
2019-02-15 19:18:38 +0400

    messages list and message view functions

--- controllers/admins.controller.php
+++ controllers/admins.controller.php
@@ -1,18 +1,50 @@
 <?php
 
-class PagesController extends Controller{
-    
-    public function index() {       
-        $this->data['test_content'] = 'pagescontroller->index';        
+class AdminsController extends Controller{
+    public function index() {
+          Router::redirect('/admins/login');
+    }
+    public function __construct(){
+        session_start();
+        if(!isset($_SESSION['admin']) && $_SERVER['REQUEST_URI'] != '/admins/login' ){
+            Router::redirect('/admins/login');
+        }
     }
     
-    public function view() {
-        $params = App::getRouter()->getParams();
-        if(isset($params[0])){
-            $alias = strtolower($params[0]);
-            $this->data['content'] =  "we on page with '{$alias}' alias";
-        }        
-        
+    public function login() {
+        if(isset($_SESSION['admin'])){
+             Router::redirect('/admins/messages');
+        }
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $data = $_POST;
+            $res = Admin::checkAdmin($data);
+            if($res['success'] == false){
+                if($res['message'] == 'no_user'){
+                    $_SESSION['message']['error'] = 'Login is Incorrect';
+                }else{
+                    $_SESSION['message']['error'] = 'Password is Incorrect';
+                }
+            }else{
+                $_SESSION['admin']['login'] = $res['login'];
+                Router::redirect('/admins/messages');
+            }
+        }
     }
     
+    public function messages() {
+        $messages = Message::getAll();
+        $this->data['messages'] = $messages;
+    }
+    
+    public function message() {
+        $id = App::getRouter()->getParams();
+        $message = Message::getByID($id);
+        var_dump($message);die;
+        $this->data['messages'] = $messages;
+    }
+    
+    public function logout() {
+        session_destroy();
+        outer::redirect('/');
+    }
 }
