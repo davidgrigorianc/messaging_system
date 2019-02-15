9400e1664f2f415caa5996b9eb38377aadab1337
davit <david.grigorianc@gmail.com>
2019-02-15 19:14:12 +0400

    messaging functions

---
+++ controllers/messages.controller.php
@@ -0,0 +1,21 @@
+<?php
+
+class MessagesController extends Controller{
+    
+    public function create(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $data = $_POST;      
+            $res =  Message::createNew($data);
+            session_start();
+            if($res){
+                $_SESSION['message']['success'] = 'Posted your message successfully!';
+            }else{
+                $_SESSION['message']['error'] = 'Sorry there was an error sending your form.';
+            }
+        }
+        Router::redirect('/');
+
+        
+    }
+    
+}
--- models/Message.php
+++ models/Message.php
@@ -6,10 +6,25 @@ class Message extends Model{
      *
      * @return array
      */
-    public static function getAll()
-    {
+    public static function getAll(){
         $db = static::getDB();
-        $stmt = $db->query('SELECT * FROM messages');
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt = $db->prepare('SELECT id, firstname, lastname, email, created, LEFT(message, 600) AS message  FROM messages');
+        $res = $stmt->execute();
+        return  $stmt->fetchAll();
+    }
+    
+    public static function createNew($args) {
+        $db = static::getDB();
+        $args['created'] = date('Y-m-d H:i:s');
+        $stmt = $db->prepare('INSERT INTO messages (firstname, lastname, email, message, created) VALUES (:firstname, :lastname, :email, :message, :created)');
+        $res = $stmt->execute($args);
+        return $res;
+    }
+    
+    public function getByID($id){
+        $db = static::getDB();
+        $stmt = $db->prepare("SELECT * FROM messages WHERE id = ? LIMIT 1");
+        $res = $stmt->execute($id);    
+        return $stmt->fetch();
     }
 }
